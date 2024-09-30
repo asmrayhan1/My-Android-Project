d409ae27a53dec999d08092fb2b433f6d8297fe3
asmrayhan1 <chowdhuryr828@gmail.com>
2024-09-30 22:22:30 +0600

    First Update

--- app/src/main/java/com/example/myjavaproject/MainActivity.java
+++ app/src/main/java/com/example/myjavaproject/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myjavaproject;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mcheckBox, mradioButton, mratingBar, mseekBar, mswitch, mform;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,55 @@ public class MainActivity extends AppCompatActivity {
 
         // For Toast
         Toast.makeText(MainActivity.this, "Welcome My Android Application.", Toast.LENGTH_SHORT).show();
+
+        mcheckBox = findViewById(R.id.checkbox);
+        mradioButton = findViewById(R.id.radiobutton);
+        mratingBar = findViewById(R.id.ratingbar);
+        mseekBar = findViewById(R.id.seekbar);
+        mswitch = findViewById(R.id.switchid);
+        mform = findViewById(R.id.formbutton);
+
+        mcheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Check_box.class));
+            }
+        });
+
+        mradioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Radio_button.class));
+            }
+        });
+
+        mratingBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Rating_bar.class));
+            }
+        });
+
+        mseekBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Seek_bar.class));
+            }
+        });
+
+        mswitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Switch_id.class));
+            }
+        });
+
+        mform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegexValidation.class));
+            }
+        });
+
     }
 }
